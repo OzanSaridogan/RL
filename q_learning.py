d7a0474566a5311eae54754bc96353bb67fbcc2d
atesozturk <131335714+aatesozturk@users.noreply.github.com>
2024-11-30 11:34:15 +0300

    Update q_learning.py
    q learning eklendi

--- q_learning.py
+++ q_learning.py
@@ -1,16 +1,10 @@
+import math
 import random
 from typing import Optional
 import numpy as np
 import gymnasium as gym
 
 class DuopolyEnv(gym.Env):
-
-    """
-    The Cornout Duopoly
-
-    ### Description:
-    There are 2 firms and these firms are 
-    """
 
     def __init__(self, a=400, b=1, cost0=20, cost1=20):
         super(DuopolyEnv, self).__init__()
@@ -29,34 +23,28 @@ class DuopolyEnv(gym.Env):
         self._profit0 = 0.0
         self._profit1 = 0.0
 
-        self._max_production = a/2 
+        self._max_production = (self.a-self.cost0)/2 if (self.cost0 < self.cost1) else (self.a-self.cost1)/2
 
         self.turn = 0
 
         # observation space quantity0, quantity1, profit0, profit1
         self.observation_space = gym.spaces.Box(
-            low=np.array([0, 0, -self._max_profit, -self._max_profit]),
-            high=np.array([self._max_production, self._max_production, self._max_profit, self._max_profit]),
+            low=np.array([0, 0], dtype=np.float64),
+            high=np.array([self._max_production, self._max_production], dtype=np.float64),
             dtype=np.float64
         )
 
-        # action space, integer
-        # it will allow a firm to make an action either decrease by max 25 or increase by max 25
-        self.action_space = gym.spaces.Box(
-            low = -25,
-            high = 25,
-            shape=(1,),
-            dtype = np.int64
-        )
+        # action space 51 discrete actions from -25 to 25
+        self.action_space = gym.spaces.Discrete(51) 
 
     def step(self, action):
-        
         if self.turn == 0:
             expected_quantity = (self.a - self.b * self._quantity1 - self.cost0) / (2 * self.b)
             expected_profit = (self.a - self.b * (expected_quantity + self._quantity1) - self.cost0) * expected_quantity
 
-            # quantity doesnt go negative
-            self._quantity0 = max(self._quantity0 + action, 0)
+            # action is between -25 25
+            action_value = (action - 25)
+            self._quantity0 = np.clip(self._quantity0 + action_value, 0, self._max_production)
             self._profit0 = (self.a - self.b * (self._quantity0 + self._quantity1) - self.cost0) * self._quantity0
 
             reward = self._profit0 - expected_profit
@@ -65,13 +53,13 @@ class DuopolyEnv(gym.Env):
             expected_quantity = (self.a - self.b * self._quantity0 - self.cost1) / (2 * self.b)
             expected_profit = (self.a - self.b * (expected_quantity + self._quantity0) - self.cost1) * expected_quantity
 
-            # quantity doesnt go negative
-            self._quantity1 = max(self._quantity1 + action, 0)
+            action_value = (action - 25)
+            self._quantity1 = np.clip(self._quantity1 + action_value, 0, self._max_production)
             self._profit1 = (self.a - self.b * (self._quantity0 + self._quantity1) - self.cost1) * self._quantity1
 
             reward = self._profit1 - expected_profit
 
-        terminated = (self._quantity0 == self._quantity1)
+        terminated = ((abs(self._quantity0 - self._quantity1) < 1) and self._quantity0 > 100)
         truncated = False
 
         observation = self._get_obs()
@@ -94,7 +82,7 @@ class DuopolyEnv(gym.Env):
         return observation, info
     
     def _get_obs(self):
-        return np.array([self._quantity0, self._quantity1, self._profit0, self._profit1], dtype=np.float64)
+        return np.array([self._quantity0, self._quantity1], dtype=np.float64)
     
     def _get_info(self):
         return {"quantity_difference" : self._quantity0 - self._quantity1}
@@ -109,3 +97,47 @@ gym.register(
 
 env = gym.make("gymnasium_env/Duopoly-v0")
 
+discrete_os_size = [190] * len(env.observation_space.high)
+
+discrete_os_win_size = (env.observation_space.high - env.observation_space.low) / discrete_os_size
+
+#q_table = np.random.uniform(low=-2, high=0, size=(discrete_os_size + [51]))
+q_table = np.zeros((discrete_os_size + [51]))
+
+learning_rate = 0.1
+discount = 0.95
+episodes = 50
+epsilon = 0.5
+
+def get_discrete_state(state):
+    discrete_state = ((state[0] - env.observation_space.low) / discrete_os_win_size).astype(np.int64)
+    discrete_state = np.clip(discrete_state, 0, np.array(discrete_os_size) - 1)
+    return tuple(discrete_state)
+
+for episode in range(episodes):
+    discrete_state = get_discrete_state(env.reset())
+    done = False
+    while not done:
+        if random.uniform(0, 1) < epsilon: # exploit
+            action = env.action_space.sample()
+        else:  # exploitation
+            action = np.argmax(q_table[discrete_state])
+
+        observation, reward, terminated, truncated, info = env.step(action)
+        new_discrete_state = get_discrete_state(observation)
+        
+
+        if not terminated:
+            max_future_q = np.max(q_table[new_discrete_state])
+            current_q = q_table[discrete_state + (action, )]
+
+            new_q = (1 - learning_rate) * current_q + learning_rate * (reward + discount * max_future_q)
+            q_table[discrete_state + (action, )] = new_q
+
+        discrete_state = new_discrete_state
+
+        episode_over = terminated or truncated
+        if episode_over:
+            break
+    env.render()
+    print("done")
